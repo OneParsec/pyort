e8ea1331582080e4c6364690a3d1f6131ff4291b
Alexandr Simonov <asimonov1998@gmail.com>
2020-12-13 11:48:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@ pip install -r requirements.txt
 pyinstaller -F pyort.py
 cd dist
 sudo cp pyort /usr/bin/
+echo "Cleaning cache!"
 cd ..
 rm -r build dist pyort.spec
+cd ..
+rm -r pyort
 echo "Installation is complete!"
